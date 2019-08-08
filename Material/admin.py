e80efa822f465f073d121ba44a3b32cabb96f4a5
Rarblack <bagiyevaydin@gmail.com>
2019-08-08 15:40:19 +0400

    SOLVE: Update issues admin panel REARRANGE: if debug on then use sqlite3, otherwise postgresql ADD: FCM notification sender to update views and admin panel save_model

--- Material/admin.py
+++ Material/admin.py
@@ -2,24 +2,26 @@ from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 from django.utils import timezone
 from django.contrib.auth.models import User
-from django.utils.translation import gettext, gettext_lazy as _
+
+from fcm_django.models import FCMDevice
+
 from .models import Profile, RequestModel, DeviceModel, PartModel, WorkPlaceModel
 
 
 @admin.register(RequestModel)
 class RequestAdmin(admin.ModelAdmin):
 
-    list_display = ('id', 'status')
+    list_display = ('id', 'sap_number', 'manufacturer', 'status', 'creator')
 
     def save_model(self, request, obj, form, change):
         if not change:
             obj.creator = request.user
             obj.creation_datetime = timezone.now()
-            super().save_model(request, obj, form, change)
+        super().save_model(request, obj, form, change)
 
 
 @admin.register(DeviceModel)
-class RequestAdmin(admin.ModelAdmin):
+class DeviceAdmin(admin.ModelAdmin):
 
     list_display = ('id', 'name')
 
@@ -27,31 +29,34 @@ class RequestAdmin(admin.ModelAdmin):
         if not change:
             obj.creator = request.user
             obj.creation_datetime = timezone.now()
-            super().save_model(request, obj, form, change)
+        elif change:
+            device = FCMDevice.objects.all().first()
+            device.send_message(title="TEST", body="THIS IS A TEST MESSAGE", data={"KEY": "THIS IS A TEST MESSAGE"})
+        super().save_model(request, obj, form, change)
 
 
 @admin.register(PartModel)
 class PartAdmin(admin.ModelAdmin):
 
-    list_display = ('id', 'name')
+    list_display = ('id', 'name', 'creator')
 
     def save_model(self, request, obj, form, change):
         if not change:
             obj.creator = request.user
             obj.creation_datetime = timezone.now()
-            super().save_model(request, obj, form, change)
+        super().save_model(request, obj, form, change)
 
 
 @admin.register(WorkPlaceModel)
-class RequestAdmin(admin.ModelAdmin):
+class WorkPlaceAdmin(admin.ModelAdmin):
 
-    list_display = ('id', 'name')
+    list_display = ('id', 'name', 'creator')
 
     def save_model(self, request, obj, form, change):
         if not change:
             obj.creator = request.user
             obj.creation_datetime = timezone.now()
-            super().save_model(request, obj, form, change)
+        super().save_model(request, obj, form, change)
 
 
 class ProfileInline(admin.StackedInline):
@@ -63,12 +68,12 @@ class ProfileInline(admin.StackedInline):
 
 class CustomUserAdmin(UserAdmin):
     inlines = (ProfileInline, )
-    list_display = ('id', 'username', 'email', 'first_name', 'last_name', 'is_staff',)
+    list_display = ('id', 'username', 'email', 'first_name', 'last_name', 'is_staff')
 
     def get_inline_instances(self, request, obj=None):
         if not obj:
             return list()
-        return super(CustomUserAdmin, self).get_inline_instances(request, obj)
+        return super().get_inline_instances(request, obj)
 
 
 admin.site.unregister(User)
