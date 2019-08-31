eecb1c4c47dfd1eb00e3039694cdbca09b261f98
Rarblack <bagiyevaydin@gmail.com>
2019-08-31 12:58:51 +0400

    ADD: filter to many to many field(parts) in EquipmentAdmin

--- EquipmentPartRequestApp/admin/equipment_admin.py
+++ EquipmentPartRequestApp/admin/equipment_admin.py
@@ -4,12 +4,18 @@ from django.utils import timezone
 from fcm_django.models import FCMDevice
 
 from ..models.equipment_models import EquipmentModel
+from ..models.part_models import PartModel
 
 
 @admin.register(EquipmentModel)
 class EquipmentAdmin(admin.ModelAdmin):
 
-    list_display = ('id', 'name')
+    list_display = ('id', 'name', 'creator', 'creation_datetime')
+
+    def formfield_for_manytomany(self, db_field, request, **kwargs):
+        if db_field.name == "parts":
+            kwargs["queryset"] = PartModel.objects.filter(creator=2)
+        return super().formfield_for_manytomany(db_field, request, **kwargs)
 
     def save_model(self, request, obj, form, change):
         if not change:
@@ -19,4 +25,3 @@ class EquipmentAdmin(admin.ModelAdmin):
             device = FCMDevice.objects.all().first()
             device.send_message(title="TEST", body="THIS IS A TEST dsaf MESSAGE", data={"KEY": "THIS IS A TEST MESSAGE"})
         super().save_model(request, obj, form, change)
-
