64e801bacd2570dfe8cf90bc41b7de1094e70460
Rarblack <bagiyevaydin@gmail.com>
2019-10-03 11:21:30 +0400

    MODIFY settings.py for server deployment usage

--- WarehouseAPI/settings.py
+++ WarehouseAPI/settings.py
@@ -91,7 +91,7 @@ WSGI_APPLICATION = 'WarehouseAPI.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
-if DEBUG:
+if not DEBUG:
     DATABASES = {
         'default': {
             'ENGINE': 'django.db.backends.sqlite3',
@@ -145,12 +145,16 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
-STATIC_URL = '/static/'
-STATIC_ROOT = '/static/'
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'EquipmentPartRequestApp/static/'),
-    os.path.join(BASE_DIR, 'static'),
-]
+
+if DEBUG:
+    STATIC_URL = '/static/'
+    STATIC_ROOT = '/static/'
+else:
+    STATIC_URL = '/static_all/'
+    STATICFILES_DIRS = [
+        os.path.join(BASE_DIR, 'EquipmentPartRequestApp/static/'),
+        os.path.join(BASE_DIR, 'static'),
+    ]
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
