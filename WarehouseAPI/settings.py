4f4477fc673a98766426e6ae6d08aa285e38d564
Rarblack <bagiyevaydin@gmail.com>
2019-10-03 11:22:00 +0400

    MODIFY settings.py for server deployment usage

--- WarehouseAPI/settings.py
+++ WarehouseAPI/settings.py
@@ -91,7 +91,7 @@ WSGI_APPLICATION = 'WarehouseAPI.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
-if not DEBUG:
+if DEBUG:
     DATABASES = {
         'default': {
             'ENGINE': 'django.db.backends.sqlite3',
